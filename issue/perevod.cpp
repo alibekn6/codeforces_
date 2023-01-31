c7d3f9e6d0d38e5b56ab633469b285be2e970ffd
alibekN54 <alibekandanuarbek@gmail.com>
2023-01-31 17:45:43 +0600

    understood the if else with >? :

--- issue/perevod.cpp
+++ issue/perevod.cpp
@@ -3,11 +3,29 @@
 #include <iostream>
 using namespace std;
 
+
 int main()
 {
+    string s;
+    string t;
+    string temp;
+    cin >> s >> t;
+    int len = size(s) - 1;
+    for (int i = len; i >= 0; i--) {
+        temp += s[i];
+    }
 
-    string word;
-    cin >> word;
-    cout << word;
+    // if ()
 
 }
+
+// int main()
+// {
+
+//     string word;
+//     cin >> word;
+//     int len =  size(word) - 1; // code -> 6
+//     for (int i = len; i >= 0; i--) {
+//         cout << word[i];
+//     }
+// }
---
+++ trainee.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int smth;
+    int y = 10;
+
+    // if (y < 10) {
+    //     smth = 12;   
+    // } else {
+    //     smth = 40;
+    // }
+
+    smth = (y == 11) ? 30 : 40;
+
+    cout << smth;
+
+}
