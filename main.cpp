c4a2bba7fa89dfbbf687dfaa023a4bc0d9185dc0
alibekN54 <alibekandanuarbek@gmail.com>
2023-01-31 17:22:29 +0600

    first commit

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    // n - напиток
+    float res;
+    int n;
+    cin >> n;
+    int nums;
+    for (int i = 0; i < n; i++) {
+        cin >> nums;
+        res += nums;
+    }
+
+    cout << res / n;
+}
