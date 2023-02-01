dab6e3e6f669477dc498a254118e00468bae4134
alibekN54 <alibekandanuarbek@gmail.com>
2023-02-01 23:01:18 +0600

    constructors

--- server/main.cpp
+++ server/main.cpp
@@ -5,21 +5,40 @@ class Employee {
 public:
     string Name;
     string Company;
-    int age;
+    int Age;
     
-    
+    Employee(string name, string company, int age) {
+        Name = name;
+        Company = company;
+        Age = age;
+    }
+
+    void intro() {
+        cout << "Name: " << Name << endl;
+        cout << "Age: " << Age << endl;
+        cout << "Company : " << Company << endl;
+    }
 
 
 };
 
 int main()
 {
-    int number;
-    Employee employee1;
-    employee1.Name = "ALibeK";
-    employee1.Company = "Google-Leetcode";
-    employee1.age = 27;
-    
-    return 0;
+    Employee emp1 = Employee("Alibek", "LeetCode", 21);
+    emp1.intro();
+
+//     int number;
+//     Employee employee1;
+//     employee1.Name = "ALibeK";
+//     employee1.Company = "Google-Leetcode";
+//     employee1.age = 27;
+//     employee1.intro();
+
+//     Employee employee2;
+//     employee1.Name = "Jony";
+//     employee2.age = 32;
+//     employee2.Company = "OlpqQ";
+
+    // employee2.intro();
 
 }
