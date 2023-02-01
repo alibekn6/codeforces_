fdb17ff6051e042cd0165c50937c8ea80600fe5b
alibekN54 <alibekandanuarbek@gmail.com>
2023-02-01 22:53:39 +0600

    C++ OOP

--- server/main.cpp
+++ server/main.cpp
@@ -1,9 +1,25 @@
 #include <iostream>
 using namespace std;
 
+class Employee {
+public:
+    string Name;
+    string Company;
+    int age;
+    
+    
+
+
+};
+
 int main()
 {
-    cout << "Hello World!";
+    int number;
+    Employee employee1;
+    employee1.Name = "ALibeK";
+    employee1.Company = "Google-Leetcode";
+    employee1.age = 27;
+    
     return 0;
 
 }
